cee18d394418862bd4b521d2324a31b39dee8da1
Sheri Warren <swarren35@yahoo.com>
2021-10-03 13:41:20 -0400

    navigate from home page to page with issues displayed

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,21 @@
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    console.log(repoName);
+    if(repoName) {
+    //display repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var displayWarning = function(repo) {
     //add text to warning container
@@ -33,9 +49,10 @@ var getRepoIssues = function(repo) {
         }
     
         else {
-        alert("There was a problem with your request!");
+        //if not successful, redirect to homepage
+        document.location.replace("./index.html");
     }
-    console.log(repo);
+   
 });
 
 }
@@ -78,4 +95,5 @@ var displayIssues = function(issues) {
     }
 
 };
-getRepoIssues("facebook/react");
+
+getRepoName();
