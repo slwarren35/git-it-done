2f67568aadb63a39f334de150258cbfb6f73faaa
Sheri Warren <swarren35@yahoo.com>
2021-10-03 20:42:43 -0400

    feature added to search by languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,6 +8,7 @@ var userFormEl = document.querySelector("#user-form");
 //targets the username entered in the search box
 var nameInputEl = document.querySelector("#username");
 
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 
 var getUserRepos = function(user) {
@@ -99,4 +100,30 @@ var displayRepos = function(repos, searchTerm) {
     }
 }
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    });
+  };
+
+  var buttonClickHandler = function(event) {
+      var language = event.target.getAttribute("data-language");
+      console.log(language);
+      if (language) {
+          getFeaturedRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent="";
+      }
+  }
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
